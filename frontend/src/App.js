a95fdf2e655f081ff7e08fde381b2186d539f4a0
anubhavvs <anubhavvsaha2001@gmail.com>
2020-10-09 23:39:20 +0530

    Added Home Page and Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main>
         <Container className='py-3'>
-          <h1>Hello World!</h1>
+          <Home />
         </Container>
       </main>
       <Footer />
