e4287d3d5716e572f9aae49cf653ef8c8d2a07f2
anubhavvs <anubhavvsaha2001@gmail.com>
2020-10-11 16:55:04 +0530

    Implemented React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Product from './pages/Product';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <Container className='py-3'>
-          <Home />
+          <Route path='/' component={Home} exact />
+          <Route path='/product/:id' component={Product} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
