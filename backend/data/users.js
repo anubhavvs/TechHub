344d70328fa7560660fe494435310f56f2d175eb
anubhavvs <anubhavvsaha2001@gmail.com>
2020-10-19 19:49:38 +0530

    UI #2

--- backend/data/users.js
+++ backend/data/users.js
@@ -3,18 +3,13 @@ import bcrypt from 'bcryptjs';
 const users = [
   {
     name: 'Admin User',
-    email: 'admin@example.com',
+    email: 'admin@techhub.com',
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
   {
-    name: 'John Doe',
-    email: 'john@example.com',
-    password: bcrypt.hashSync('123456', 10),
-  },
-  {
-    name: 'Jane Doe',
-    email: 'jane@example.com',
+    name: 'Anubhav Saha',
+    email: 'anubhavvs@techhub.com',
     password: bcrypt.hashSync('123456', 10),
   },
 ];
